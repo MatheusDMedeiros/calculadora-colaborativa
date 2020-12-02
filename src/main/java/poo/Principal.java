2db1319008d18c368cb201c0687dc5602ab6e01c
Matheus Medeiros <medeirosmatheus0705@gmail.com>
2020-12-02 18:12:12 -0300

    Alterando Principal.java

--- src/main/java/poo/Principal.java
+++ src/main/java/poo/Principal.java
@@ -11,10 +11,6 @@ public class Principal {
         return a * b;
     }
 
-    public static int mult(int a, int b){
-
-        return a * b;
-    }
 
     // metodo subtracao
     public static int sub(int a, int b){
